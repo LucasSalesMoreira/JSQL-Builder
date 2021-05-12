a30d08c8b35919137636abe9e87fa57e2aa7b57a
LucasSalesMoreira <lucas.sales@mc2performa.com.br>
2021-05-12 17:23:44 -0300

    First commit

---
+++ src/com/github/lucassalesmoreira/jsqlbuilder/base/Query.java
@@ -0,0 +1,4 @@
+package com.github.lucassalesmoreira.jsqlbuilder.base;
+
+public class Query {
+}
---
+++ src/com/github/lucassalesmoreira/jsqlbuilder/structure/Table.java
@@ -0,0 +1,9 @@
+package com.github.lucassalesmoreira.jsqlbuilder.structure;
+
+import com.github.lucassalesmoreira.jsqlbuilder.base.Query;
+
+public class Table extends Query {
+    public Table(String tableName) {
+
+    }
+}
